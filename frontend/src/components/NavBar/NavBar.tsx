bfe8ab9f62ae380dc3de6a43fd351652309a5bff
MaximoHumittas <maxmora401@gmail.com>
2025-04-20 02:47:33 -0400

    listo para el deploy

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import './NavBar.css'; // Importando el archivo de estilos
